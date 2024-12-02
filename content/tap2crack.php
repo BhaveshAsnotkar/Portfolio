9512374e598a8d31a4578780557e6cff667dea6a
Bhavesh Asnotkar <bhaveshasnotkar@gmail.com>
2024-12-02 18:21:54 +0530

    t2c assessment

---
+++ content/tap2crack.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UX / UI Designer - Bhavesh Asnotkar</title>
+
+	<!-- meta tags	 -->
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="../images/favicon.png"/>
+
+	<!-- meta tags	 -->
+
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/fontawesome.css">
+</head>
+<div id="loader"></div>
+
+<body class="dark-theme">
+
+	<nav>
+		<a href="../index.php">
+			<img src="../images/logo-white.png" class="logo desktop-logo">
+			<img src="../images/logo-mobile.png" class="logo mobile-logo">
+		</a>
+		<div id="nav-icon">
+			<div class="hamburger">
+				<div class="nav-circuit"><span class="ellipse"></span><span class="line"></span><span class="ellipse"></span></div>
+				<div class="nav-circuit"><span class="ellipse"></span><span class="line"></span><span class="ellipse"></span></div>
+				<div class="nav-circuit"><span class="ellipse"></span><span class="line"></span><span class="ellipse"></span></div>
+			</div>
+		</div>
+		<ul class="menu-items">
+			<li><a href="../index.php#portfolio">Portfolio</a></li>
+			<li><a href="../index.php#skills">Skills </a></li>
+			<!-- <li><a href="#">About</a></li> -->
+			<li><a href="../index.php#contact">Contact</a></li>
+		</ul>		
+	</nav>
+
+	
+
+<section class="case-study-section-new">
+
+
+	<div class="section-title-new">
+
+		<div class="banner-stayfit rems">
+			
+			<div>
+				<video class="video_t2c"  autoplay muted >
+				  <source src="../video/t2c.mp4" type="video/mp4" >
+				</video>
+			</div>
+
+			<div>
+				<h3>Assessment </h3>
+				<h2>Tap2Crack - EDU</h2>
+				<h6>Case Study</h6>
+			</div>
+			
+		</div>
+
+		<svg
+		  class="slant"
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 100 100"
+		  preserveAspectRatio="none"
+		>
+		  <polygon points="0,100 100,0 100,100" />
+		</svg>
+	</div>
+
+
+	<div class="project-description z-top">
+		<div class="new-title">
+			<h6>Assessment</h6>
+			<h4>Objective</h4>
+		</div>
+		<div class="project-description-content">
+			<p>Design a Progress Tracking Dashboard for mobile and web, allowing users to track learning progress, performance, and areas for improvement.</p>
+		</div>
+	</div>
+
+	<div>
+
+	<svg
+		  class="slant2"
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 100 100"
+		  preserveAspectRatio="none"
+		>
+		  <polygon points="0,100 100,0 100,100" />
+		</svg>
+
+	</div>
+
+	<div class="persona-new">
+		<div class="new-title">
+			<h6>UX</h6>
+			<h4>USER PERSONA</h4>
+		</div>
+
+		<div class="persona-content">
+			<div class="persona-image">
+				<img src="../images/persona/m4.png">
+				<p>Akash Singh</p>
+			</div>
+
+			<div class="persona-details">
+				<h4>15 / SSC Student / Mumbai </h4>
+				<p>Akash is preparing for SSC exams while balancing school. He relies on a mobile app to track progress, focus on weak areas, and stay motivated with gamification.</p>
+			</div>
+		</div>
+
+		<div class="persona-content reverse">
+			<div class="persona-image">
+				<img src="../images/persona/f6.png">
+				<p>Sunita Gupta</p>
+			</div>
+
+			<div class="persona-details">
+				<h4>42 / Homemaker / Delhi</h4>
+				<p>Sunita is a homemaker monitoring her son’s SSC preparation. She needs a simple mobile dashboard to track his progress, tasks, and weak areas with clear visuals and updates.</p>
+			</div>
+		</div>
+
+	</div>
+
+
+	<!-- <div class="user-flow-new">
+		<div class="new-title">
+			<h6>UX</h6>
+			<h4>USER Flow</h4>
+		</div>
+
+		<img src="../images/rems/user-flow.png"  width="100%">
+
+	</div>
+
+
+	<div>
+		<svg
+		  class="slant3"
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 100 100"
+		  preserveAspectRatio="none"
+		>
+		  <polygon points="0,100 100,0 100,100" />
+		</svg>
+	</div>
+
+	<div class="typography-new">
+		<div class="new-title">
+			<h6>Ui</h6>
+			<h4>Typography</h4>
+		</div>
+
+		<img src="../images/wps/typo.png"  width="100%">
+
+
+	</div>
+
+	<div class="colors-new">
+		<div class="new-title">
+			<h6>Ui</h6>
+			<h4>Colors</h4>
+		</div>
+
+		<img src="../images/wps/color.png">	
+
+	</div> -->
+
+	
+
+	<div class="colors-new pb-100">
+		<div class="new-title">
+			<h6>Ui</h6>
+			<h4>High Fidelity Wireframes</h4>
+		</div>
+		<div class="high-fedility-new">
+			<img src="../images/t2c/wireframe3.png" class="">
+			<img src="../images/t2c/wireframe1.png" class="">
+		</div>
+	</div>
+
+
+	
+		<svg
+		  class="slant4 mobile-hide"
+		  xmlns="http://www.w3.org/2000/svg"
+		  viewBox="0 0 100 100"
+		  preserveAspectRatio="none"
+		>
+		  <polygon points="0,100 100,0 100,100" />
+		</svg>
+	
+	<div class="view-website-button stayfit">
+		<div class="duo_pro_btn">
+			<a target="_blank" href="https://www.figma.com/proto/ADpxBT2P7zTxiuHHPCdBf5/Tap2Crack-Assessment?node-id=1-3&node-type=frame&t=jmFVw2qPkD0QmWgr-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A3">Desktop Prototype</a>
+			<a target="_blank" href="https://www.figma.com/proto/ADpxBT2P7zTxiuHHPCdBf5/Tap2Crack-Assessment?node-id=101-1295&node-type=frame&t=eZDBVdxnFqkCITKv-1&scaling=scale-down&content-scaling=fixed&page-id=101%3A1290&starting-point-node-id=101%3A1295">Mobile Prototype</a>
+		</div>
+		<img src="../images/t2c/probaner.png" class="bottom_img_t2c_mobile mobile-hide">
+	</div>
+	
+
+</section>
+
+
+
+
+
+
+
+<script src="../js/jquery.min.js"></script>
+
+<script src="../js/myscript.js"></script>
+
+</body>
+</html>
